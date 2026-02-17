96822c15f3f12ddf499527bc254dc9551cfe406b
suryadeepr <suryadeep.r@freshworks.com>
2026-02-17 14:49:44 +0530

    Revert "updating log line (#50)"
    This reverts commit 3e8f4c4d323171c2950142aa122132a42febc770.

--- source/extensions/filters/network/redis_proxy/command_splitter_impl.cc
+++ source/extensions/filters/network/redis_proxy/command_splitter_impl.cc
@@ -1902,8 +1902,6 @@ SplitRequestPtr InstanceImpl::makeRequest(Common::Redis::RespValuePtr&& request,
     }
   }
   ENVOY_LOG(info, "command to process '{}'- sent from remote client ip '{}'", request->toString(),stream_info.downstreamAddressProvider().remoteAddress()->asString());
-  
-  ENVOY_LOG(warn, "cmd - '{}'", request->toString().substr(0, 50));
   // Extract command name
   std::string command_name = absl::AsciiStrToLower(request->asArray()[0].asString());
 
